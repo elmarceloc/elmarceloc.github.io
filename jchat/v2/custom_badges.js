fcb01d3ac3e111f79f55d3f351b63d67e87da74a
elmarceloc <73359763+elmarceloc@users.noreply.github.com>
2022-05-12 19:47:17 -0400

    a

--- jchat/v2/custom_badges.js
+++ jchat/v2/custom_badges.js
@@ -1,22 +1,3 @@
 var booyahtv_badges = [
-    {
-        name: 'elmarceloc',
-        url: 'https://cdn.betterttv.net/emote/620d959d06fd6a9f5be4c2a7/3x'
-    },
-    {
-        name: 'aless_kc',
-        url: 'https://cdn.betterttv.net/emote/621fca1f06fd6a9f5be60535/3x'
-    },
-    {
-        name: 'BlupBlup_o:',
-        url: 'https://cdn.betterttv.net/emote/606ebcacfba15a03df2ca6c2/3x'
-    },
-    {
-        name: 'notvalegangas',
-        url: 'https://cdn.betterttv.net/emote/60a7489567644f1d67e8a245/3x'
-    },
-    {
-        name: 'nonsito',
-        url: 'https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_8fface1224134bcb8f2935cb7fcb3cea/default/dark/3.0',
-    }
+
 ]
